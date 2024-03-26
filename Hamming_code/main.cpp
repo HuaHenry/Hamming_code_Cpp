fad5fda852e86ad70ef220b8e79657e657cabd6d
Zhouqi HUA <henryhua0721@foxmail.com>
2024-03-26 10:36:40 +0800

    Initial Commit

---
+++ Hamming_code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hamming_code
+//
+//  Created by 华洲琦 on 2024/3/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
